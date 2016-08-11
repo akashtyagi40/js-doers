3b005c1910f162907cedb7ece29fa4627c0109fe
akash <akash@retechlabs.com>
2016-08-11 16:34:12 +0530

    first commit --skip-ci

--- call-utility.js
+++
@@ -1,3 +0,0 @@
-//Calling map function
-JSON.stringify([1,2,3].map(function(x) { return x + 1; }));
-
---
+++ main.js
@@ -0,0 +1,9 @@
+
+var requirejs = require('requirejs');
+var Task = require("./task");
+
+console.log("abc");
+
+//Calling map function
+/*JSON.stringify([1,2,3].map(function(x) { return x + 1; }));*/
+
